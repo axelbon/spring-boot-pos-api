28c9caff16f8d35851628c883f31133246b66f2f
axelbon <axel.bon.cc@gmail.com>
2025-11-10 15:55:21 -0600

    Add: initial spring boot project

---
+++ src/test/java/com/axelbon/pos/PosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axelbon.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
