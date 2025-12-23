ac3f6c9f6be7dc19b217b8297019fd164b646fc5
axelbon <axel.bon.cc@gmail.com>
2025-12-23 11:52:03 -0600

    feat: add category entity and repository

---
+++ src/main/java/com/axelbon/pos/entity/Category.java
@@ -0,0 +1,47 @@
+package com.axelbon.pos.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SoftDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@SoftDelete
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name="categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable= false, unique= true)
+    private String name;
+    
+    private String description;
+
+    @CreationTimestamp
+    @Column(nullable=false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+}
---
+++ src/main/java/com/axelbon/pos/repository/CategoryRepository.java
@@ -0,0 +1,16 @@
+package com.axelbon.pos.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.axelbon.pos.entity.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+    
+    // SELECT * FROM categories WHERE name = ? AND id != ?
+    Optional<Category> findByNameAndIdNot(String name, Long id);
+}
